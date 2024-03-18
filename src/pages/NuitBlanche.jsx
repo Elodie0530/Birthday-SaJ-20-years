b4487f6a755111a63bed7b8d531e094f13c2800a
Elodie0530 <uneondine@gmail.com>
2024-03-18 14:13:55 +0100

    add texts from posters white night

--- src/pages/NuitBlanche.jsx
+++ src/pages/NuitBlanche.jsx
@@ -1,87 +1,169 @@
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
-// import nb15 from "../assets/2015_affiche_nuit_blanche.png";
+import "./NuitBlanche.css";
 
 function NuitBlanche() {
   return (
     <>
-      <Navbar />
-      <h1>La Convention `Nuit Blanche sur l'Alsace`</h1>
-      <div>
-        <p>
-          Voici la Convention majeur de jeux de Rôles de La Sauce aux Jeux.
-          <br></br>
-          Principalement dans l'univers de Cthulhu (univers d’Howard Phillips
-          Lovecraft), ainsi que l’histoire et la culture du Bas-Rhin.<br></br>
-          Elle existe depuis l'été 2015 jusqu'à nos jours.<br></br> Elle tend à
-          évoluée et se diversifiée avec les associations partenaires, en
-          prenant une nouvelle forme, donc rendez-vous est donner pour la
-          prochaine édition les 22 et 23 juin 2024. <br></br>
-          <br></br>
-          Retour en affiches et en chiffres sur les Conventions passées :
-        </p>
+      <div className="nuitBlanche">
+        <Navbar />
+        <h1>La Convention `Nuit Blanche sur l'Alsace`</h1>
         <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2015_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2015"
-          />
+          <p>
+            Voici la Convention majeur de jeux de Rôles de La Sauce aux Jeux.
+          </p>
+          <p>
+            Principalement dans l'univers de Cthulhu (créer par Howard Phillips
+            Lovecraft), ainsi que l’histoire et la culture du Bas-Rhin. Avec des
+            scénarios spécialement constituer pour cette occasion.
+            <p>
+              Cette convention gratuite sur inscription, existe depuis l'été
+              2015 jusqu'à nos jours.
+            </p>
+            <p>
+              Elle tend à évoluée et se diversifiée avec les associations
+              partenaires, en prenant une nouvelle forme, donc rendez-vous est
+              donner pour la prochaine édition les 22 et 23 juin 2024.
+            </p>
+            Retour en affiches et en chiffres sur les Conventions des années passées :
+          </p>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2015_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2015"
+            />
+            <p>
+              Dans la nuit du samedi 29 au 30 août 2015,<br></br> dans les
+              jardins du Conseil Départemental du Bas-Rhin
+            </p>
+            <p>
+              18 tables de jeux de rôles ont été proposées soit 88 joueurs pour
+              la première édition
+            </p>
+          </div>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2016_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2016"
+            />
+            <p>
+              Dans la nuit du samedi 27 au 28 août 2016,<br></br> au Fort Rapp
+              Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la deuxième édition, il y avait 91 joueurs avec 19 tables
+              de jeux de rôles, ainsi qu'une soirée enquête “Intrigue au Manoir
+              de Kintzheim” proposée par l'association des Tisseurs d'Etoiles
+            </p>
+          </div>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2017_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2017"
+            />
+            <p>
+              Dans la nuit du samedi 24 au 25 juin 2017,<br></br> au Fort Rapp
+              Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la troisième édition, il y avait 94 joueurs avec 20 tables
+              de jeux de rôles, ainsi qu'une soirée enquête “Insmül, la cité
+              dont on ne parle pas” proposée par l'association des Tisseurs
+              d'Etoiles
+            </p>
+          </div>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2018_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2018"
+            />
+            <p>
+              Dans la nuit du samedi 23 au 24 juin 2018,<br></br> au Fort Rapp
+              Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la quatrième édition, il y avait 121 joueurs avec 23
+              tables de jeux de rôles, ainsi qu'une soirée enquête "Le drame
+              d'Ensisheim" proposée par l'association des Tisseurs d'Etoiles et
+              des animations costumées par l'association des ForgeMondes
+            </p>
+          </div>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2019_affiche_nuit_blanche.png"}
+              alt="nuitblanche2019"
+            />
+            <p>
+              Dans la nuit du samedi 29 au 30 juin 2019,<br></br> au Fort Rapp
+              Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la cinquième édition, il y avait 110 joueurs avec 26
+              tables de jeux de rôles. <br></br> Avec comme particularité pour
+              les Maîtres du jeu, soit de créer un nouveau scénario, soit de
+              faire rejouer celui d'une année précédente.<br></br> Sur les 26
+              tables de jeux seuls 11 sont des sénorios ayant déjà été proposés.
+            </p>
+          </div>
+
+          {/* image masque année 2020 covid pandémie nationale */}
+
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2021_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2021"
+            />
+            <p>
+              Dans la nuit du samedi 03 au 04 juillet 2021,<br></br> au Fort
+              Rapp Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la sixième édition, il y avait 75 joueurs avec 17 tables
+              de jeux de rôles. <br></br> Les différentes tables seront lancées
+              en décalées avec la mise en place d'un protocole sanitaire
+            </p>
+          </div>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2022_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2022"
+            />
+            <p>
+              Dans la nuit du samedi 25 au 26 juin 2022,<br></br> au Fort Rapp
+              Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la septième édition, il y avait 61 joueurs avec 15 tables
+              de jeux de rôles.<br></br> Avec des animations costumées par
+              l'association des ForgeMondes
+            </p>
+          </div>
+          <div>
+            <img
+              className="posterWhiteNight"
+              src={"src/assets/2023_affiche_nuit_blanche.jpeg"}
+              alt="nuitblanche2023"
+            />
+            <p>
+              Dans la nuit du samedi 24 au 25 juin 2023,<br></br> au Fort Rapp
+              Moltke de Reichstett
+            </p>
+            <p>
+              Lors de la huitième édition, il y avait 69 joueurs avec 16 tables
+              de jeux de rôles.<br></br> Avec des animations costumées par
+              l'association des ForgeMondes
+            </p>
+          </div>
         </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2016_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2016"
-          />
-        </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2017_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2017"
-          />
-        </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2018_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2018"
-          />
-        </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2019_affiche_nuit_blanche.png"}
-            alt="nuitblanche2019"
-          />
-        </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2021_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2021"
-          />
-        </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2022_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2022"
-          />
-        </div>
-        <div>
-          <img
-            className="posterWhiteNight"
-            src={"src/assets/2023_affiche_nuit_blanche.jpeg"}
-            alt="nuitblanche2023"
-          />
-        </div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
-        eligendi ipsum asperiores atque dignissimos, ipsa itaque, maiores
-        recusandae debitis tenetur ab labore ducimus deserunt eos enim maxime
-        aliquam sunt odit!
+        <Footer />
       </div>
-      <Footer />
     </>
   );
 }
