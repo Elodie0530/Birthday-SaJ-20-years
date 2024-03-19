969792bb8bdb31f8e50211689b038044a6573fef
Elodie0530 <uneondine@gmail.com>
2024-03-19 17:12:47 +0100

    change order text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,12 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <p>Réalisé en mars 2024 par Elodie Jehl</p>
       <div>
         <Link to="/credits" className="credits">
           Crédits
         </Link>
       </div>
+      <p>Réalisé en mars 2024 par Elodie Jehl</p>
     </div>
   );
 }
